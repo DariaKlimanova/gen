52652b5c9579fa3fb6341792b22d6ad98457f1de
DariaKlimanova <66948219+DariaKlimanova@users.noreply.github.com>
2020-09-22 11:19:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import *
 
 
-def generator_pr():
+def generator_pr(): # формирование списка простых чисел с помощью решета Эратосфена
     n = 2000
     numbers = list(range(2, n + 1))
     for number in numbers:
@@ -11,37 +11,37 @@ def generator_pr():
     return list(filter(lambda x: x != 0, numbers))
 
 
-def generate():
+def generate(): # генерация рандомного числа в указанном диапазоне
     p = randint(0, 10000000000000000000)
-    p = p | (1 << 0)
+    p = p | (1 << 0) # приведение числа к нечетной форме
     return p
 
 
-def proverka():
+def proverka(): 
     p = generate()
     lp = generator_pr()
     i = 0
-    while i < len(lp):
-        if p % lp[i] == 0 and p != lp[i]:
-            p = generate()
-            i = 0
+    while i < len(lp): # в цикле делим сгенерированное число на каждый элемент списка простых чисел;
+        if p % lp[i] == 0 and p != lp[i]: # если число разделится на простое, не равное себе,
+            p = generate() # то запускается генерация нового числа 
+            i = 0 # счетчик прохода по списку сбрасывается
         else:
             i += 1
-    return p
+    return p # в итоге вернем число, прошедшее проверку делением на каждое число из списка
 
 
-def miller(p):
+def miller(p): # алгоритм Рабина-Миллера
     b = 0
     m = p - 1
     while m % 2 == 0:
-        b, m = b + 1, m / 2
-    for i in range(5):
+        b, m = b + 1, m / 2 # ищем b через наибольшее число делений (p-1) на 2
+    for i in range(5): # повторить 5 раз
         a = randint(1, p - 1)
         z = pow(int(a), int(m), p)  # z = a^m mod p
         if z == 1 or z == p - 1:
             continue  # p может быть простым
         for r in range(1, b):
-            z = (z * z) % p
+            z = (z * z) % p # z^2 mod p
             if z == 1:
                 return 'False'
             if z == p - 1:
